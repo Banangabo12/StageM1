773c03c8bdbed460555fb231e8ffb86c34796f4f
Aristote <theophilentoumba@icloud.com>
2024-04-24 16:51:01 +0200

    Calcule de l'accroissement en biomasse

--- p.R
+++ p.R
@@ -106,31 +106,31 @@ install.packages("httr2")
   
   # Calcule de l'accroissement en  biomasse en fontion de l'espéce sans information sur la hauteur entre 1991-2022
   
-  paracoutest  %>%
+  paracoutest %>%
     select(idTree, `1991`, `2022`, Species) %>% 
-    mutate(paracou_agbfT1) %>% 
-    mutate(paracou_agbfT) %>% 
-    mutate(accroissement = `2022` - `1991`) %>% 
+    mutate(biomasse_22 = paracou_agbfT$biomasse) %>%
+    mutate(biomasse_91 = paracou_agbfT1$biomasse) %>% 
+    mutate(accroissement = `biomasse_22` - `biomasse_91`) %>% 
     filter(!is.na(accroissement)) %>% 
-    group_by(Genus,Species) %>%
-    summarise(accroissement_sp = mean(accroissement) / 31) %>% 
+    group_by(Species) %>%
+    # summarise(accroissement_sp = mean(accroissement) / 31) %>% 
     print() ->
     paracou2
   
   
   
   
-  # Calcule de l'accroissement en biomasse en fonction de la taille sans information sur la hauteur
+    # Calcule de l'accroissement en biomasse en fonction de la taille sans information sur la hauteur
   
   
-  paracoutest  %>%
-    select(idTree, `1991`, `2022`, Species) %>% 
-    mutate(paracou_agbfT1) %>% 
-    mutate(paracou_agbfT) %>% 
-    mutate(accroissement = `2022` - `1991`) %>% 
+  paracoutest %>%
+    select(idTree, `1991`, `2022`, diametre_2022) %>% 
+    mutate(biomasse_22 = paracou_agbfT$biomasse) %>%
+    mutate(biomasse_91 = paracou_agbfT1$biomasse) %>% 
+    mutate(accroissement = `biomasse_22` - `biomasse_91`) %>% 
     filter(!is.na(accroissement)) %>% 
     group_by(idTree,diametre_2022) %>%
-    summarise(accroissement_sp = mean(accroissement) / 31) %>% 
+    # summarise(accroissement_sp = mean(accroissement) / 31) %>% 
     print() ->
     paracou3
   
@@ -138,250 +138,4 @@ install.packages("httr2")
   
 
   
-   ## Calcule de la biomasse avec la hauteur. NB: ce n'est pas dans les objectifs
-  
-  #Calcule de la hauteur avec les données Chave et al. (2012) tout en utilisant l'equation 6
-  
-  
-  
    
-  dataHchave <- retrieveH(
-    D = paracoutest $ diametre_2022,
-    coord = paracoutest[, c("Lon", "Lat")]
-  )
-  
-  
-  
-  
-  #Ajout de mean et H
-  
-  paracoutest %>% 
-    mutate(dataWD$meanWD) %>% 
-    mutate(dataHchave$H) %>% 
-    print () -> 
-    paracou_agb
-  
-  
-  
-  #Calcule de la biomasse par arbre
-  
-  AGBtree <- computeAGB(
-    D = paracou_agb$diametre_2022,
-    WD = paracou_agb$`dataWD$meanWD`,
-    H = paracou_agb$`dataHchave$H`
-  )
-  
-  print(AGBtree)
-  
-  
-  paracou_agb  %>% 
-    mutate(AGBtree) %>% 
-    print () -> 
-    paracou_agb_t
-  
-  
-  
-  
-  
-  
-  
-  
- 
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-   
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
